d5ea728775d072164dee87888e307c3063fbb52b
System Administrator <mandl.cho@icloud.com>
2025-11-27 00:27:12 +0800

    Render sample blueprint via template

--- blueprint-vibe/src/App.tsx
+++ blueprint-vibe/src/App.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useRef } from "react";
 import "ueblueprint/dist/css/ueb-style.min.css";
 import "ueblueprint/dist/ueblueprint.js";
 import { SAMPLE_BLUEPRINT } from "./sampleBlueprint";
@@ -12,28 +11,12 @@ declare global {
 }
 
 function App() {
-  const blueprintRef = useRef<HTMLElement | null>(null);
-
-  useEffect(() => {
-    const el = blueprintRef.current;
-    if (!el) return;
-
-    const mount = async () => {
-      if (typeof customElements?.whenDefined === "function") {
-        await customElements.whenDefined("ueb-blueprint");
-      }
-      el.innerHTML = `<template>\n${SAMPLE_BLUEPRINT}\n</template>`;
-    };
-
-    mount();
-  }, []);
-
   return (
-    <div className="app-root">
-      <code>
-        <ueb-blueprint ref={blueprintRef}></ueb-blueprint>
-      </code>
-    </div>
+    <code>
+      <ueb-blueprint>
+        <template dangerouslySetInnerHTML={{ __html: SAMPLE_BLUEPRINT }} />
+      </ueb-blueprint>
+    </code>
   );
 }
 
